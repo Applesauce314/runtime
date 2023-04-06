08084606c91891446a3632662f6c122002dce57b
Levi Broderick <levib@microsoft.com>
2023-04-06 16:49:20 -0700

    Remove DllMain from zlib allocator

--- src/native/external/zlib/dotnet_allocator_win.c
+++ src/native/external/zlib/dotnet_allocator_win.c
@@ -4,50 +4,55 @@
 #include <Windows.h>
 #include <heapapi.h>
 #include <intsafe.h>
+#include <winnt.h>
 #include "zutil.h"
 
-// This is the special heap we'll allocate from.
-HANDLE s_allocHeap = NULL;
-
-BOOL WINAPI DllMain(
-    _In_ HINSTANCE hinstDLL,
-    _In_ DWORD     fdwReason,
-    _In_ LPVOID    lpvReserved
-)
+// Gets the special heap we'll allocate from.
+HANDLE GetZlibHeap()
 {
-    BOOL retVal = TRUE;
+#ifdef _WIN64
+    static HANDLE s_hPublishedHeap = NULL;
 
-    switch (fdwReason)
+    // If already initialized, return immediately.
+    // We don't need a volatile read here since the publish is performed with release semantics.
+    if (s_hPublishedHeap != NULL) { return s_hPublishedHeap; }
+
+    // Attempt to create a new heap. If we can't, fall back to the standard process heap.
+    // The heap will be dynamically sized.
+    BOOL fDefaultedToProcessHeap = FALSE;
+    HANDLE hNewHeap = HeapCreate(0, 0, 0);
+
+    if (hNewHeap != NULL)
     {
-        case DLL_PROCESS_ATTACH:
-            // Attempt to create a new heap. If we can't, fall back to the standard process heap.
-            s_allocHeap = HeapCreate(0, 0, 0);
-            if (s_allocHeap != NULL)
-            {
-                // Attempt to set the LFH flag on our new heap. Since it's just an optimization, swallow failures.
-                // Ref: https://learn.microsoft.com/windows/win32/api/heapapi/nf-heapapi-heapsetinformation
-                ULONG ulHeapInformation = 2; // LFH
-                HeapSetInformation(s_allocHeap, HeapCompatibilityInformation, &ulHeapInformation, sizeof(ulHeapInformation));
-            }
-            else
-            {
-                s_allocHeap = GetProcessHeap();
-            }
-            break;
-
-        case DLL_PROCESS_DETACH:
-            if (s_allocHeap != NULL && s_allocHeap != GetProcessHeap())
-            {
-                retVal = HeapDestroy(s_allocHeap);
-                s_allocHeap = NULL;
-            }
-            break;
-
-        default:
-            break; // We don't care about thread notifications.
+        // Attempt to set the LFH flag on our new heap. Since it's just an optimization, ignore failures.
+        // Ref: https://learn.microsoft.com/windows/win32/api/heapapi/nf-heapapi-heapsetinformation
+        ULONG ulHeapInformation = 2; // LFH
+        HeapSetInformation(hNewHeap, HeapCompatibilityInformation, &ulHeapInformation, sizeof(ulHeapInformation));
+    }
+    else
+    {
+        hNewHeap = GetProcessHeap();
+        fDefaultedToProcessHeap = TRUE;
     }
 
-    return retVal;
+    HANDLE hExistingPublishedHeap = InterlockedCompareExchangePointer(&s_hPublishedHeap, hNewHeap, NULL);
+    if (hExistingPublishedHeap == NULL)
+    {
+        // We successfully published our heap handle - use it.
+        return hNewHeap;
+    }
+    else
+    {
+        // Another thread already created the heap handle and published it.
+        // We should destroy any custom heap we created and fall back to the existing published handle.
+        if (!fDefaultedToProcessHeap) { HeapDestroy(hNewHeap); }
+        return hExistingPublishedHeap;
+    }
+#else
+    // We don't want to create a new heap in a 32-bit process because it could end up
+    // reserving too much of the address space. Instead, fall back to the normal process heap.
+    return GetProcessHeap();
+#endif
 }
 
 typedef struct _DOTNET_ALLOC_COOKIE
@@ -71,7 +76,7 @@ voidpf ZLIB_INTERNAL zcalloc (opaque, items, size)
     unsigned items;
     unsigned size;
 {
-    (void)opaque;
+    (void)opaque; // suppress C4100 - unreferenced formal parameter
 
     // If initializing a fixed-size structure, zero the memory.
     DWORD dwFlags = (items == 1) ? HEAP_ZERO_MEMORY : 0;
@@ -92,7 +97,7 @@ voidpf ZLIB_INTERNAL zcalloc (opaque, items, size)
     SIZE_T cbActualAllocationSize;
     if (FAILED(SizeTAdd(cbRequested, DOTNET_ALLOC_HEADER_COOKIE_SIZE_WITH_PADDING + DOTNET_ALLOC_TRAILER_COOKIE_SIZE, &cbActualAllocationSize))) { return NULL; }
 
-    LPVOID pAlloced = HeapAlloc(s_allocHeap, dwFlags, cbActualAllocationSize);
+    LPVOID pAlloced = HeapAlloc(GetZlibHeap(), dwFlags, cbActualAllocationSize);
     if (pAlloced == NULL) { return NULL; } // OOM
 
     // Now set the header & trailer cookies
@@ -128,7 +133,7 @@ void ZLIB_INTERNAL zcfree (opaque, ptr)
     voidpf opaque;
     voidpf ptr;
 {
-    (void)opaque;
+    (void)opaque; // suppress C4100 - unreferenced formal parameter
 
     if (ptr == NULL) { return; } // ok to free nullptr
 
@@ -147,7 +152,7 @@ void ZLIB_INTERNAL zcfree (opaque, ptr)
     zcfree_trash_cookie(pHeaderCookie);
     zcfree_trash_cookie(pTrailerCookie);
 
-    if (!HeapFree(s_allocHeap, 0, pHeaderCookie)) { goto Fail; }
+    if (!HeapFree(GetZlibHeap(), 0, pHeaderCookie)) { goto Fail; }
     return;
 
 Fail:
